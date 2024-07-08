e56c28c2de9291da60ee4806262dfa6a95a52725
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-07-08 14:52:40 +0200

    chore(login): add google login

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import vue3GoogleLogin from 'vue3-google-login'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,9 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vue3GoogleLogin, {
+    clientId: '642667688966-urrgb8an0706ts0tu8mr48j5eqkvne2e.apps.googleusercontent.com'
+})
 app.use(router)
 
 app.mount('#app')
---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { type CallbackTypes, GoogleLogin } from "vue3-google-login";
+import { decodeCredential } from "vue3-google-login";
+
+const callback: CallbackTypes.CredentialCallback = (response) => {
+  const userData = decodeCredential(response.credential);
+  console.log("Handle the response", response);
+  console.log("💅 decoded user data", userData);
+};
+</script>
+
+<template>
+  <GoogleLogin :callback="callback" prompt auto-login />
+</template>
