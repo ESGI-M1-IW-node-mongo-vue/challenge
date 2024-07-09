485aa76a3fbc9c84f13f9006ce289c6688523be7
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-07-09 09:56:46 +0200

    move login

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import { decodeCredential, GoogleLogin } from "vue3-google-login";
-import type { CallbackTypes } from "vue3-google-login";
-
-const callback: CallbackTypes.CredentialCallback = (response) => {
-  const userData = decodeCredential(response.credential);
-  console.log("Handle the response", response);
-  console.log("💅 decoded user data", userData);
-};
 </script>
 
 <template>
