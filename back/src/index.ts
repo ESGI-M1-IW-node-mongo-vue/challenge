279428b1b0ed5b27da889f051576094856a88200
Lucas <ldp030602@gmail.com>
2024-07-09 10:46:53 +0200

    Add artist route

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,24 +1,25 @@
-import {serve} from '@hono/node-server'
-import {Hono} from 'hono'
-import {DbConnect} from './db'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { DbConnect } from "./db";
 import styles from "./routes/styles";
+import artists from "./routes/artists";
 
+const app = new Hono();
+await DbConnect();
 
-const app = new Hono()
-await DbConnect()
-
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 // 3000/api/styles
 
-app.route('/api', styles)
+app.route("/api", styles);
+app.route("/api", artists);
 
 app.all("*", (c) => {
-  return c.json({msg: '404 oups'})
+  return c.json({ msg: "404 oups" });
 });
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
---
+++ back/src/routes/artists.ts
@@ -0,0 +1,77 @@
+import { Hono } from "hono";
+import { Artist } from "../models/artists";
+import { isValidObjectId } from "mongoose";
+
+const api = new Hono().basePath("/artists");
+
+api.get("/", async (c) => {
+  return c.json(await Artist.find());
+});
+
+api.get("/:id", async (c) => {
+  const _id = c.req.param("id");
+
+  if (isValidObjectId(_id)) {
+    const oneArt = await Artist.findOne({ _id });
+    return c.json(oneArt);
+  }
+  return c.json({ msg: "ObjectId malformed" }, 400);
+});
+
+api.post("/", async (c) => {
+  const body = await c.req.json();
+  try {
+    const newArt = new Artist(body);
+    const saveArt = await newArt.save();
+    return c.json(saveArt, 201);
+  } catch (error: any) {
+    return c.json(error._message, 400);
+  }
+});
+
+api.put("/:id", async (c) => {
+  const _id = c.req.param("id");
+  const body = await c.req.json();
+  const q = {
+    _id,
+  };
+  const updateQuery = {
+    ...body,
+  };
+  const tryToUpdate = await Artist.findOneAndUpdate(q, updateQuery, {
+    new: true,
+  });
+  return c.json(tryToUpdate, 200);
+});
+
+api.patch("/:id", async (c) => {
+  const _id = c.req.param("id");
+  const body = await c.req.json();
+  const q = {
+    _id,
+  };
+  const { categories, ...rest } = body;
+
+  const updateQuery = {
+    $addToSet: {
+      categories: categories,
+    },
+    $set: { ...rest },
+  };
+  const tryToUpdate = await Artist.findOneAndUpdate(q, updateQuery, {
+    new: true,
+  });
+  return c.json(tryToUpdate, 200);
+});
+
+api.delete("/:id", async (c) => {
+  const _id = c.req.param("id");
+  const tryToDelete = await Artist.deleteOne({ _id });
+  const { deletedCount } = tryToDelete;
+  if (deletedCount) {
+    return c.json({ msg: "DELETE done" });
+  }
+  return c.json({ msg: "not found" }, 404);
+});
+
+export default api;
