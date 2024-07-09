fd7bd091f8f8f0aee3c022f2035068679937b77e
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-07-09 16:05:33 +0200

    feat(search): connect the styles to the search and dynamic search button

--- back/src/models/styles.ts
+++ back/src/models/styles.ts
@@ -1,15 +1,15 @@
-import {model, Schema} from 'mongoose';
-
+import { model, Schema } from "mongoose";
 
 export interface IStyle {
-    name: string;
+  name: string;
+  image?: string;
 }
 
 const styleSchema = new Schema<IStyle>({
-    name: {type: String, required: true, lowercase: true, trim: true},
+  name: { type: String, required: true, lowercase: true, trim: true },
+  image: { type: String },
 });
 
+const Style = model<IStyle>("styles", styleSchema);
 
-const Style = model<IStyle>('styles', styleSchema);
-
-export {Style}
+export { Style };
