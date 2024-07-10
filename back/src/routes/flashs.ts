e539e71a337bf87cedb19dcee3051e2b43fab45e
Lucas <ldp030602@gmail.com>
2024-07-10 12:00:26 +0200

    feat: add s3 save

--- back/src/routes/flashs.ts
+++ back/src/routes/flashs.ts
@@ -1,21 +1,23 @@
-import {Hono} from "hono";
-import {Flash} from "../models/flashs";
+import { Hono } from "hono";
+import { Flash } from "../models/flashs";
+import { SaveOnS3 } from "../aws-s3";
 
 const api = new Hono().basePath("/flashs");
 
 api.get("/", async (c) => {
-    return c.json(await Flash.find());
+  return c.json(await Flash.find());
 });
 
 api.post("/", async (c) => {
-    const body = await c.req.json();
-    try {
-        const newFlash = new Flash(body);
-        const saveFlash = await newFlash.save();
-        return c.json(saveFlash, 201);
-    } catch (error: any) {
-        return c.json(error._message, 400);
-    }
+  const body = await c.req.parseBody();
+  try {
+    body.img = await SaveOnS3(body["img"] as File);
+    const newFlash = new Flash(body);
+    const saveFlash = await newFlash.save();
+    return c.json(saveFlash, 201);
+  } catch (error: any) {
+    return c.json(error._message, 400);
+  }
 });
 
-export default api
+export default api;
