b4b2e24287d6c4af03a4474adc40069adfa1c484
RuddyMo <ruddymorel10@gmail.com>
2024-07-10 10:35:15 +0200

    feat(result): add dynamic

---
+++ front/src/components/ArtistsCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <article class="flex flex-col gap-6 bg-white">
+    <img
+        class="h-48 object-cover object-center"
+        :src="artist.image"
+        :alt="artist.name"
+    />
+    <div class="flex flex-col gap-6 bg-white p-5">
+      <p class="text-xl font-semibold text-center">{{ artist.name }}</p>
+      <div class="flex gap-4 justify-center">
+        <div class="flex gap-2">
+          <svg-icon
+              type="mdi"
+              :path="mdiMapMarker"
+              class="text-orange-500"
+          ></svg-icon>
+          <p>{{ artist.city }}</p>
+        </div>
+        <p>|</p>
+        <div class="flex gap-2">
+          <svg-icon
+              type="mdi"
+              :path="mdiStar"
+              class="text-orange-500"
+          ></svg-icon>
+          <p>{{ artist.rating }} ({{ artist.reviews }} avis)</p>
+        </div>
+      </div>
+      <p class="mx-4 sm:mx-10">{{ artist.description }}</p>
+      <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
+        <button
+            v-for="style in artist.styles"
+            :key="style"
+            class="px-4 py-2 border rounded-full text-gray-700 bg-white"
+        >
+          {{ style }}
+        </button>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script setup>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiStar, mdiMapMarker } from "@mdi/js";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  artist: {
+    type: Object,
+    required: true
+  }
+});
+</script>
--- front/src/views/ResultSearch.vue
+++ front/src/views/ResultSearch.vue
@@ -1,13 +1,7 @@
-<script setup>
-import SvgIcon from "@jamescoyle/vue-icon";
-import { mdiStar } from "@mdi/js";
-import { mdiMapMarker } from "@mdi/js";
-</script>
-
 <template>
   <main class="bg-gray-100">
     <div
-      class="bg-[url('.././assets/img/searchBackgroud.png')] h-72 flex items-center justify-center"
+        class="bg-[url('.././assets/img/searchBackgroud.png')] h-72 flex items-center justify-center"
     >
       <h1 class="text-3xl text-white">Trouvez un tatoueur près de chez vous</h1>
     </div>
@@ -18,147 +12,14 @@ import { mdiMapMarker } from "@mdi/js";
           <div>
             <h2 class="text-2xl font-semibold">Tatoueurs en France</h2>
             <p class="text-lg">
-              <span class="text-orange-500">2912</span> tatoueurs trouvés
+              <span class="text-orange-500">{{ allArtists.length }}</span> tatoueurs trouvés
             </p>
           </div>
           <div class="mt-2 mb-5">
             <hr class="h-0 bg-slate-400 rounded-lg" />
 
             <div class="grid grid-cols-1 sm:grid-cols-2 mt-6 gap-4">
-              <article class="flex flex-col gap-6 bg-white">
-                <img
-                  class="h-48 object-cover object-center"
-                  src="../assets/img/exempleArtist.jpg"
-                  alt=""
-                />
-                <div class="flex flex-col gap-6 bg-white p-5">
-                  <p class="text-xl font-semibold text-center">Elena</p>
-                  <div class="flex gap-4 justify-center">
-                    <div class="flex gap-2">
-                      <svg-icon
-                        type="mdi"
-                        :path="mdiMapMarker"
-                        class="text-orange-500"
-                      ></svg-icon>
-                      <p>Annecy</p>
-                    </div>
-                    <p>|</p>
-                    <div class="flex gap-2">
-                      <svg-icon
-                        type="mdi"
-                        :path="mdiStar"
-                        class="text-orange-500"
-                      ></svg-icon>
-                      <p>5.0 (7 avis)</p>
-                    </div>
-                  </div>
-                  <p class="mx-4 sm:mx-10">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Assumenda, repellendus dolorem, deleniti sint accusamus ad
-                    nemo numquam tempore provident, nobis maiores inventore non
-                    natus vero?
-                  </p>
-                  <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                  </div>
-                </div>
-              </article>
-              <article class="flex flex-col gap-6 bg-white">
-                <img
-                  class="h-48 object-cover object-center"
-                  src="../assets/img/exempleArtist.jpg"
-                  alt=""
-                />
-                <div class="flex flex-col gap-6 bg-white p-5">
-                  <p class="text-xl font-semibold text-center">Elena</p>
-                  <div class="flex gap-4 justify-center">
-                    <div class="flex gap-2">
-                      <svg-icon
-                        type="mdi"
-                        :path="mdiMapMarker"
-                        class="text-orange-500"
-                      ></svg-icon>
-                      <p>Annecy</p>
-                    </div>
-                    <p>|</p>
-                    <div class="flex gap-2">
-                      <svg-icon
-                        type="mdi"
-                        :path="mdiStar"
-                        class="text-orange-500"
-                      ></svg-icon>
-                      <p>5.0 (7 avis)</p>
-                    </div>
-                  </div>
-                  <p class="mx-4 sm:mx-10">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Assumenda, repellendus dolorem, deleniti sint accusamus ad
-                    nemo numquam tempore provident, nobis maiores inventore non
-                    natus vero?
-                  </p>
-                  <div class="grid grid-cols-2 sm:grid-cols-4 gap-2 text-sm">
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                    <button
-                      class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-                    >
-                      Animal
-                    </button>
-                  </div>
-                </div>
-              </article>
+              <ArtistCard v-for="artist in allArtists" :key="artist.id" :artist="artist" />
             </div>
           </div>
         </div>
@@ -169,49 +30,11 @@ import { mdiMapMarker } from "@mdi/js";
           <hr class="h-0 bg-slate-400 rounded-lg mt-2" />
           <article class="flex flex-wrap gap-1 text-sm pt-5">
             <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
+                v-for="style in styles"
+                :key="style.id"
+                class="px-4 py-2 border rounded-full text-gray-700 bg-white"
             >
-              test
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
-            </button>
-            <button
-              class="px-4 py-2 border rounded-full text-gray-700 bg-white"
-            >
-              Animal
+              {{ style.name }}
             </button>
           </article>
         </div>
@@ -219,3 +42,21 @@ import { mdiMapMarker } from "@mdi/js";
     </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import ArtistCard from "../components/ArtistsCard.vue";
+const styles = ref([]);
+const allArtists = ref([]);
+
+fetch("http://localhost:3000/api/styles")
+    .then((res) => res.json())
+    .then((data) => (styles.value = data));
+
+fetch("http://localhost:3000/api/artists")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Artists data:", data);
+      allArtists.value = data;
+    });
+</script>
