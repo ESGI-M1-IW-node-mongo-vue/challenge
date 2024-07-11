72faf6dbc4798d2ce4f20574cd831cd8633b4e15
RuddyMo <ruddymorel10@gmail.com>
2024-07-11 11:12:14 +0200

    feat(search): fix searchbar

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -5,9 +5,9 @@
       aria-label="Global"
     >
       <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
+        <a href="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
-          <img class="h-8 w-auto" src="../assets/img/logo.png" alt="" />
+          <img class="h-14 w-auto" src="../assets/img/logo.png" alt="" />
         </a>
       </div>
       <div class="flex lg:hidden">
